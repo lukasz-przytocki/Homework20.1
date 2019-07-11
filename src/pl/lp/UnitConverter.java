e8a11ecae3c73f2aadc4a1b2141488904388b5fc
lukasz-przytocki <49994174+lukasz-przytocki@users.noreply.github.com>
2019-07-11 11:37:21 +0200

    Update UnitConverter.java

--- src/pl/lp/UnitConverter.java
+++ src/pl/lp/UnitConverter.java
@@ -23,8 +23,65 @@ public class UnitConverter extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.println(metry+" "+centymetry+" "+milimetry);
 
+        if (((metry != "" && metry != null) && (centymetry != "" && centymetry != null))
+                || ((metry != "" && metry != null) && (milimetry != "" && milimetry != null))
+                || ((centymetry != "" && centymetry != null) && (milimetry != "" && milimetry != null))
+                || ((kilogramy != "" && kilogramy != null) && (gramy != "" && gramy != null))
+                || ((kilogramy != "" && kilogramy != null) && (miligramy != "" && miligramy != null))
+                || ((gramy != "" && gramy != null) && (miligramy != "" && miligramy != null))) {
+            writer.println("<h2> Należy wypełnić tylko jedno pole</h2>");
+        } else {
+
+            if (metry != "" && metry != null) {
+                double doubleMetry = Double.valueOf(metry);
+                double doubleCentymetry = doubleMetry * 100;
+                double doubleMilimetry = doubleMetry * 1000;
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>metry: " + doubleMetry + "<br><br>" + "centymetry: " + doubleCentymetry + "<br><br>" + "milimetry:" + doubleMilimetry + "<h2>");
+            }
+            if (centymetry != "" && centymetry != null) {
+                double doubleCentymetry = Double.valueOf(centymetry);
+                double doubleMetry = doubleCentymetry / 100;
+                double doubleMilimetry = doubleCentymetry * 10;
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>metry: " + doubleMetry + "<br><br>" + "centymetry: " + doubleCentymetry + "<br><br>" + "milimetry:" + doubleMilimetry + "<h2>");
+            }
+
+            if (milimetry != "" && milimetry != null) {
+                double doubleMilimetry = Double.valueOf(milimetry);
+                double doubleCentymetry = doubleMilimetry / 10;
+                double doubleMetry = doubleMilimetry / 1000;
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>metry: " + doubleMetry + "<br><br>" + "centymetry: " + doubleCentymetry + "<br><br>" + "milimetry:" + doubleMilimetry + "<h2>");
+            }
+
+            if (kilogramy != "" && kilogramy != null) {
+                double doubleKilogramy = Double.valueOf(kilogramy);
+                double doubleGramy = doubleKilogramy * 1000;
+                double doubleMiligramy = doubleKilogramy * 1_000_000;
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>kilogramy: " + doubleKilogramy + "<br><br>" + "gramy: " + doubleGramy + "<br><br>" + "miligramy:" + doubleMiligramy + "<h2>");
+            }
+
+            if (gramy != "" && gramy != null) {
+                double doubleGramy = Double.valueOf(gramy);
+                double doubleKilogramy = doubleGramy / 1000;
+                double doubleMiligramy = doubleGramy * 1_000;
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>kilogramy: " + doubleKilogramy + "<br><br>" + "gramy: " + doubleGramy + "<br><br>" + "miligramy:" + doubleMiligramy + "<h2>");
+            }
+
+            if (miligramy != "" && miligramy != null) {
+                double doubleMiligramy = Double.valueOf(miligramy);
+                double doubleGramy = doubleMiligramy / 1000;
+                double doubleKilogramy = doubleMiligramy / 1_000_000;
+
+                writer.println("<h1> Podana wartość w przeliczeniu na:</h1>");
+                writer.println("<h2>kilogramy: " + doubleKilogramy + "<br><br>" + "gramy: " + doubleGramy + "<br><br>" + "miligramy:" + doubleMiligramy + "<h2>");
+            }
+
+        }
     }
 
 }
